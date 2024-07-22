626ceb1ebd70fd3e7e9769766fc8a3a1f4e51536
NicolasEche <nicolas.echeverri41@eia.edu.co>
2024-07-22 14:01:30 -0500

    Cambios cositas

--- main/static/script.js
+++ main/static/script.js
@@ -4,4 +4,25 @@ function toggleMenu(){
     var navLinks = document.getElementById('nav-items');
     navLinks.classList.toggle('navbar-nav-active');
 }
-//esto es el js
+document.addEventListener('DOMContentLoaded', function() {
+    const track = document.querySelector('.carousel-track');
+    let index = 0;
+    const slides = track.children;
+    const totalSlides = slides.length;
+    const slideWidth = slides[0].getBoundingClientRect().width;
+
+    function nextSlide() {
+        index++;
+        if (index >= totalSlides) {
+            index = 0;
+        }
+        updateCarousel();
+    }
+
+    function updateCarousel() {
+        const offset = -index * slideWidth;
+        track.style.transform = `translateX(${offset}px)`;
+    }
+
+    setInterval(nextSlide, 3000); // Cambia la imagen cada 3 segundos (3000 ms)
+});
