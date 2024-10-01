895f6ddb6e4cf0d994f188d57a015e14085abdcb
sparisca05 <simon.parisca@eia.edu.co>
2024-09-30 20:14:02 -0500

    carrusel galeria responsive

--- main/static/script.js
+++ main/static/script.js
@@ -14,17 +14,37 @@ function hideInfoApto(idTitle, idInfo){
     info.style.display = 'none';
     title.style.display = 'block';
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     const track = document.querySelector('.carousel-track');
     let index = 0;
     const slides = track.children;
     const totalSlides = slides.length;
-    const slideWidth = track.getBoundingClientRect().width / 3; // Divide el ancho total por 3
+    let windowWidth;
+    let slideWidth;
+    
+    function setSlideWidth() {
+        windowWidth = track.getBoundingClientRect().width
+        if (windowWidth > 768) {
+            slideWidth = windowWidth / 3; // Divide el ancho total por 3
+        } else {
+            slideWidth = windowWidth / 2; // Divide el ancho total por 2
+        }
+    }
+    setSlideWidth();
 
     function nextSlide() {
+        
         index += 1; // Aumenta el índice en 3 para mostrar 3 imágenes
-        if (index >= totalSlides - 2) {
-            index = 0; // Reinicia el índice si se excede
+        
+        if (windowWidth > 768) {
+            if (index >= totalSlides - 2) {
+                index = 0; // Reinicia el índice si se excede
+            }
+        } else {
+            if (index >= totalSlides - 1) {
+                index = 0; // Reinicia el índice si se excede
+            }
         }
         updateCarousel();
     }
@@ -34,5 +54,10 @@ document.addEventListener('DOMContentLoaded', function() {
         track.style.transform = `translateX(${offset}px)`;
     }
 
+    window.addEventListener('resize', function() {
+        setSlideWidth();
+        updateCarousel();
+    });
+    
     setInterval(nextSlide, 4000); // Cambia las imágenes cada 3 segundos (3000 ms)
 });
